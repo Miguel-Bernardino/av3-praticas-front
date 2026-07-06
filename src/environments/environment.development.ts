2a8235f89f067e077e0c7a675974018ac88a68ad
Miguel Bernardino Sousa Borges da Silva <miguel.bernardino.5891@gmail.com>
2026-07-06 01:20:13 +0000

    envio

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production:false,
+    endPoint: 'http://44.203.144.142:8080'
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: true,
+    endPoint: 'http://44.203.144.142:8080'
+};
